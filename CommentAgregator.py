582caf4b52563679c3a835a0c2ecf34dbffd7109
Brayden Hill <hillbgh@aggiemail.usu.edu>
2021-07-23 14:24:30 -0400

    Added comment dump functionality

--- CommentAgregator.py
+++ CommentAgregator.py
@@ -1,41 +1,50 @@
 import os
 import json
 import googleapiclient.discovery
+from urllib.parse import urlparse
 
 
 def main():
-    api_service_name = "youtube"
-    api_version = "v3"
-    DEVELOPER_KEY = config("APIKEY")
+    response, nextPageToken = apiRequest()
+    # dumpAllComments(response,nextPageToken)
 
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey=DEVELOPER_KEY)
-
-    request = youtube.commentThreads().list(
-        part='snippet',
-        videoId=Vid,
-        textFormat='plainText'
-    )
-    response = dict(request.execute())
-
-    parseJSON(response)
+    parsePrintJSON(response)
 
 
-def parseJSON(response, level=0):
+def parsePrintJSON(response, level=0):
     for key in response:
         print("\t" * level + key)
         if type(response[key]) == dict:
-            parseJSON(response[key], level + 1)
+            parsePrintJSON(response[key], level + 1)
         elif type(response[key]) == str:
             print("\t" * (level + 1) + response[key])
         elif type(response[key]) == list or type(response[key]) == tuple:
             for item in response[key]:
-                parseJSON(item, level + 1)
+                parsePrintJSON(item, level + 1)
         elif type(response[key]) != str:
             print("\t" * (level + 1) + str(response[key]))
 
 
-def config(searchString):
+def parseJSONComments(response, level=0):
+    for key in response:
+        if key == "textDisplay":
+            print(response[key])
+        if type(response[key]) == dict:
+            parseJSONComments(response[key], level + 1)
+        elif type(response[key]) == list or type(response[key]) == tuple:
+            for item in response[key]:
+                parseJSONComments(item, level + 1)
+
+
+def dumpAllComments(response="", nextPageToken=""):
+    parseJSONComments(response)
+    while nextPageToken is not None:
+        response,nextPageToken = apiRequest(nextPageToken)
+        parseJSONComments(response)
+    print("Done.")
+
+
+def configFileSeek(searchString):
     with open("config.json", "r") as configFile:
         configDict = json.load(configFile)
         if searchString in configDict:
@@ -44,7 +53,39 @@ def config(searchString):
             raise ("The", searchString, "was not found! Please check the config file!")
 
 
+def extractVID(link):
+    link = urlparse(link)
+    query = link[4]
+    return query[2:]
+
+
+def apiRequest(PageToken=""):
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = configFileSeek("APIKEY")
+
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey=DEVELOPER_KEY)
+
+    request = youtube.commentThreads().list(
+        part='snippet',
+        videoId=Vid,
+        textFormat='plainText',
+        maxResults=100,
+        pageToken=PageToken
+
+    )
+    response = dict(request.execute())
+
+    try:
+        return response, response["nextPageToken"]
+    except:
+        return response, None
+
+
+
 if __name__ == "__main__":
-    # Vid = input("Please enter the video Id you are searching for.")
-    Vid = "IqQLvlKzuX8"
+    link = input("Please enter the video Id you are searching for.")
+    Vid = extractVID(link)
+
     main()
